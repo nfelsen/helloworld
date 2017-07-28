4b1ececa116e1afc4f65418973bf32827b714d33
Nathaniel Felsen <n@medium.com>
2017-07-27 23:25:30 -0700

    Adding CloudWatch support to our application

--- helloworld.js
+++ helloworld.js
@@ -1,5 +1,10 @@
 var http = require("http")
 var winston = require("winston")
+var AWS = require("aws-sdk")
+
+AWS.config.update({region:"us-east-1"})
+var cwevents = new AWS.CloudWatchEvents({apiVersion: "2015-10-07"})
+var cw = new AWS.CloudWatch({apiVersion: "2010-08-01"})
 
 var version = process.env.HELLOWORLD_VERSION
 
@@ -18,6 +23,26 @@ logger.rewriters.push(function(level, msg, meta) {
 })
 
 http.createServer(function (request, response) {
+  var event = {
+    Entries: [{
+      Detail: JSON.stringify(request.headers),
+      DetailType: "hellworld application access request",
+      Source: "helloworld.app"
+    }]
+  }
+
+  var metric = {
+    MetricData: [{
+      MetricName: "page_viewwed",
+      Dimensions: [{
+        Name: "Version",
+        Value: version
+      }],
+      Unit: "None",
+      Value: 1.0
+    }],
+    Namespace: "Helloworld/traffic"
+  }
 
    // Send the HTTP header
    // HTTP Status: 200 : OK
@@ -26,6 +51,20 @@ http.createServer(function (request, response) {
 
    // Send the response body as "Hello World"
    response.end('Hello World\n')
+   cwevents.putEvents(event, function(err, data) {
+    if (err) {
+      logger.error("error", "an error occurred when creating an event", {error: err})
+    } else {
+      logger.info("created event", {entries: data.Entries})
+    }
+  })
+   cw.putMetricData(metric, function(err, data) {
+    if (err) {
+      logger.error("an error occurred when creating a metric", {error: err});
+    } else {
+      logger.info("created metric", {data: JSON.stringify(data)});
+    }
+  })
 }).listen(3000)
 
 // Console will print the message
